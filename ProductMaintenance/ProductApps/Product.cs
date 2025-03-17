5aed53d94c581b18e01b293baf952dcf059d7609
Abdul Ahmed <abdul.ahmed4@student.tafesa.edu.au>
2025-03-17 13:15:20 +1030

    Added $25 delivery charge to total payment calculation

--- ProductMaintenance/ProductApps/Product.cs
+++ ProductMaintenance/ProductApps/Product.cs
@@ -63,7 +63,8 @@ namespace ProductApps
         //Calculate the total payment
         public void calTotalPayment()
         {
-            TotalPayment = Price * Quantity;
+            Delivery = 25.00m; // Fixed delivery charge
+            TotalPayment = (Price * Quantity) + Delivery;
         }
     }
 }
